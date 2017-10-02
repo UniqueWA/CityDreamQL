302afa01c98897c8949e92e93ee49b27aae6a78d
mistadrumma <breakroger104@gmail.com>
2017-10-03 00:20:52 +0300

    Test

--- CityDreamQL/settings.py
+++ CityDreamQL/settings.py
@@ -22,7 +22,7 @@ class Common(Configuration):
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
 
-    ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = [*]
 
     # Application definition
     INSTALLED_APPS = [
